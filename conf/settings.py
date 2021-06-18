cc7379fbcf04b970bff0ba50c6179769aa06bd48
katefranks <kmfranks9@gmail.com>
2021-06-18 15:57:00 -0400

    updated conf settings

--- conf/settings.py
+++ conf/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'fullstack-chat-app-katefranks.herokuapp.com/',
+    '127.0.0.1',
+]
 
 
 # Application definition
