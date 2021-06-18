4bed9315aae4f30b96ed14b81cac91bc7e412f2b
katefranks <kmfranks9@gmail.com>
2021-06-18 15:42:40 -0400

    initial commit

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+
+urlpatterns = [
+    # path('menu/', include('menu.urls', namespace='menu')),
+    # path('orders/', include('orders.urls', namespace='orders')),
+]
