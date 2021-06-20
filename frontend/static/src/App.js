ab9e58beba07fbf12252fb2e3116f989437e2c3e
katefranks <kmfranks9@gmail.com>
2021-06-19 22:24:46 -0400

    css added

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     }
     this.addInstantMessage = this.addInstantMessage.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.addToChatWindow = this.addToChatWindow.bind(this);
 }
 componentDidMount(){
     fetch('/api/v1/instantMessages/')
@@ -44,25 +45,111 @@ componentDidMount(){
       .then(data => console.log(data));
   }
 
+  addToChatWindow(instantMessage) {
+    const instantMessages = [...this.state.instantMessages];
+    this.instantMessages.push(instantMessage);
+    this.setState({instantMessages});
+  }
+
   render(){
     const instantMessages = this.state.instantMessages.map(instantMessage => (
-      <li key={instantMessage.id}>
+      <li className="instant-messages" key={instantMessage.id}>
       <p>{instantMessage.created_at}</p>
       <p>{instantMessage.user_name}</p>
-      <p>{instantMessage.message_text}</p>
+      <p className="instant-message-text-display">{instantMessage.message_text}</p>
       </li>
     ))
     return(
 
-      <div>
-      <ul>{instantMessages}</ul>
+      <div className="chat-app-container">
+      <header className="chat-app-header">Instant Messenger</header>
       <form onSubmit={this.addInstantMessage}>
         <input type="text" name="user_name" value={this.state.user_name} onChange={this.handleInput} placeholder="Enter Name"/>
         <input type="text" name="message_text" value={this.state.message_text} onChange={this.handleInput} placeholder="Enter Text"/>
-        <button type="submit" value="Submit">Submit</button>
+        <button type="submit" value="Submit" onClick={() => this.state.addToChatWindow(this.instantMessages)}>Submit</button>
       </form>
+      <ul className="instant-message-window">{instantMessages}</ul>
       </div>
     )
   }
 }
 export default App;
+
+// onClick={() => this.addToChatWindow(this.newMessage)}
+/////////////////////////
+// REFACTORING
+/////////////////////////
+// import React from 'react';
+// import Cookies from 'js-cookie';
+// import './App.css';
+//
+// class App extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       instantMessages: [],
+//
+//     }
+//     this.addInstantMessage = this.addInstantMessage.bind(this);
+//     this.handleInput = this.handleInput.bind(this);
+//     // this.addToChatWindow = this.addToChatWindow.bind(this);
+// }
+// componentDidMount(){
+//     fetch('/api/v1/instantMessages/')
+//       .then(response => response.json())
+//       .then(data => this.setState({ instantMessages: data }));
+//   }
+//   handleInput(e) {
+//     this.setState({[e.target.name]: e.target.value});
+//   }
+//
+//   addInstantMessage(e){
+//     console.log(this.state.value);
+//     e.preventDefault();
+//
+//     const instantMessage = {
+//       // created_at: this.state.created_at,
+//       user_name: this.state.user_name,
+//       message_text: this.state.message_text,
+//     };
+//
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-CSRFToken': Cookies.get('csrftoken'),
+//       },
+//       body: JSON.stringify(instantMessage),
+//     }
+//     fetch('/api/v1/instantMessages/', options)
+//       .then(response => response.json())
+//       .then(data => console.log(data));
+//   }
+//   // addToChatWindow(newMessage) {
+//   //   const instantMessages = [...this.state.instantMessages];
+//   //   instantMessages.push(newMessage);
+//   //   this.setState({instantMessages});
+//   // }
+//
+//   render(){
+//     const instantMessages = this.state.instantMessages.map(instantMessage => (
+//       <li key={instantMessage.id}>
+//       <p>{instantMessage.created_at}</p>
+//       <p>{instantMessage.user_name}</p>
+//       <p>{instantMessage.message_text}</p>
+//       </li>
+//     ))
+//     return(
+//
+//       <div>
+//       <ul>{instantMessages}</ul>
+//       <form onSubmit={this.addInstantMessage}>
+//         <input type="text" name="user_name" value={this.state.user_name} onChange={this.handleInput} placeholder="Enter Name"/>
+//         <input type="text" name="message_text" value={this.state.message_text} onChange={this.handleInput} placeholder="Enter Text"/>
+//         <button type="submit" value="Submit">Submit</button>
+//       </form>
+//       </div>
+//     )
+//   }
+// }
+// export default App;
