b2985d9a738910d0934edbe475154026f72d2f71
katefranks <kmfranks9@gmail.com>
2021-08-20 15:03:52 -0400

    added demo functionality

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// import Cookies from 'js-cookie';
-
-// import './Login.css';
 
 class Login extends React.Component {
 
@@ -15,6 +12,7 @@ class Login extends React.Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.demoLogin = this.demoLogin.bind(this);
 
   }
   handleInput(e) {
@@ -26,11 +24,16 @@ class Login extends React.Component {
      this.props.handleLogin(this.state);
   }
 
+  demoLogin(e){
+  this.setState({username: 'demo', email: 'demo@demo.com', password: 'safepass1', });
+}
+
 render(){
  return (
    <>
    <form onSubmit={this.handleSubmit} className="form-login">
     <label className="login-label" htmlFor="">Login:</label>
+    <button onClick={this.demoLogin} type="button"className="submit-button">Demo</button>
     <br/>
     <input className="login-input" placeholder="username" name="username" type="text" value={this.state.username} onChange={this.handleInput}/>
     <br/>
@@ -49,75 +52,3 @@ render(){
 }
 
 export default Login;
-
-// import React from 'react';
-// import Cookies from 'js-cookie';
-//
-// class Login extends React.Component {
-//
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       username: '',
-//       email: '',
-//       password: '',
-//     }
-//
-//     this.handleLogin = this.handleLogin.bind(this);
-//     this.handleInput = this.handleInput.bind(this);
-//
-//   }
-//   handleInput(e) {
-//     this.setState({[e.target.name]: e.target.value});
-//   }
-//
-//   async handleLogin(e){
-//     e.preventDefault();
-//
-//     const user = {
-//       username: this.state.username,
-//       email: this.state.email,
-//       password: this.state.password,
-//     }
-//
-//
-//     const options = {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'X-CSRFToken': Cookies.get('csrftoken')
-//       },
-//       body: JSON.stringify(user),
-//     };
-//
-//     const handleError = (err) => console.warn(err);
-//     const response = await fetch('/rest-auth/login/', options).catch(handleError);
-//     const data = await response.json().catch(handleError);
-//
-// console.log(user);
-//   if(data.key){
-//     Cookies.set('Authorization', `Token ${data.key}`);
-//   }
-//   }
-//
-// render(){
-//  return (
-//    <form onSubmit={this.handleLogin}>
-//     <label htmlFor="">Login</label>
-//     <br/>
-//     <input className="login-input" placeholder="username" name="username" type="text" value={this.state.username} onChange={this.handleInput}/>
-//     <br/>
-//     <input className="login-input" placeholder="email" type="email" name="email" value={this.state.email} onChange={this.handleInput}/>
-//     <br/>
-//     <input className="login-input" placeholder="password" type="password" name="password" value={this.state.password} onChange={this.handleInput}/>
-//     <br/>
-//     <button type="submit">Submit</button>
-//    </form>
-//
-//  );
-//
-// }
-//
-// }
-//
-// export default Login;
